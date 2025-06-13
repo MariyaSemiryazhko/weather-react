1accffc13ae87ce9a151bc59ab6c1960f2880fe9
MariyaSemiryazhko <semiryazhkomasha@gmail.com>
2025-06-13 18:36:01 +0200

    change API integration

--- src/App.js
+++ src/App.js
@@ -2,52 +2,23 @@ import React from "react";
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Search from "./Search";
-import Temperatur from "./Temperatur";
 
-function App() {
+export default function App() {
   return (
-    <div className="container">
-      <Search />
-      <div className="today">
-        <div className="container">
-          <div className="row align-items-start">
-            <div className="col">
-              <p id="place">Munich</p>
-              <div id="date">13.05</div>
-              <Temperatur temperature={19} />
-              <div>
-                <img src="sunny.png" width="40%" id="icon" alt="clear" />
-                <br />
-              </div>
-            </div>
-
-            <div className="col-6">
-              <div>
-                Wind 💨: <span id="wind">31</span> km/h
-              </div>
-              <div>
-                Humidity 💧: <span id="humidity">80</span> %
-              </div>
-              <div id="description">Rainy</div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <h5>Weather forecast for the next 5 days:</h5>
-      <br />
-
-      <div className="weather-forecast" id="forecast"></div>
-
-      <footer>
-        <a href="https://github.com/MariyaSemiryazhko/weather-react">
+    <div className="App">
+      <div className="container">
+      <Search defaultCity="New York" />
+    <footer>
+      <a href="https://github.com/MariyaSemiryazhko/weather-react">
           Open source code
-        </a>{" "}
-        by Mariya Semiryazhko 
-        <a href="https://genuine-begonia-c88d72.netlify.app/"> on Netlify</a>
-      </footer>
+      </a>{" "}
+      by Mariya Semiryazhko
+      <a href="https://genuine-begonia-c88d72.netlify.app/"> on Netlify</a>
+    </footer>
     </div>
-  );
+    </div>
+  )
+  
 }
 
-export default App;
+
--- src/Search.js
+++ src/Search.js
@@ -1,70 +1,76 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function SearchEngine() {
-  let [city, setCity] = useState("");
-  let [weatherData, setWeatherData] = useState(null);
+export default function Search(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (!city) return;
+    function handleResponse(response) {
+        setWeatherData({
+            ready: true,
+            city: response.data.city,
+            temperature: response.data.temperature.current,
+            humidity: response.data.temperature.humidity,
+            description: response.data.condition.description,
+            wind: response.data.wind.speed,
+            icon: response.data.condition.icon_url,
+            date: "Thursday, 13.05"
+        });
+    }
 
-    const url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=21b0o158462fba6c2a0f6eca1t37c8ec&units=metric`;
-    axios.get(url).then((response) => {
-      setWeatherData({
-        city: city,
-        temperature: response.data.temperature.current,
-        humidity: response.data.temperature.humidity,
-        description: response.data.condition.description,
-        wind: response.data.wind.speed,
-        icon: response.data.condition.icon_url,
-      });
-    });
-  }
+    if (weatherData.ready) {
+        return (
+            <div className="container">
+              <form className="input-group city" id="form">
+          <input
+            type="text"
+            className="form-control"
+            id="searchEngine"
+            placeholder="Enter a city..."
+          />
+          <input type="submit" value="Search" className="search" />
+        </form>
 
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
 
-  const weatherItems = weatherData
-    ? [
-        {
-          label: "Temperature",
-          value: `${Math.round(weatherData.temperature)}°C`,
-        },
-        { label: "Humidity", value: `${weatherData.humidity}%` },
-        { label: "Description", value: weatherData.description },
-        { label: "Wind", value: `${weatherData.wind} km/h` },
-      ]
-    : [];
+                <div className="today">
+                    <div className="container">
+                        <div className="row align-items-start">
+                            <div className="col">
+                                <p id="place">{weatherData.city} </p>
+                                <div id="date">{weatherData.date}</div>
+                                 
+                                <div>Temperature🌡️: {Math.round(weatherData.temperature)}°C</div>
+                                <div>
+                                    Wind💨: <span id="wind">{weatherData.wind}</span> km/h
+                                </div>
+                                <div>
+                                    Humidity💧: <span id="humidity">{weatherData.humidity}</span> %
+                                </div>
+                                <div id="description" className="text-capitalize">{weatherData.description}</div>
+                                
+                            </div>
+            
+                            <div className="col-6">
+                            <div>
+                                    <img   className=" img-fluid img-weather" src={weatherData.icon} alt={weatherData.description} />
+                            
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="weather-forecast" id="forecast"></div>
+                
+            </div>
+        );
 
-  return (
-    <div>
-      <form className="input-group city" id="form" onSubmit={handleSubmit}>
-        <input
-         type="text"
-         className="form-control"
-         id="searchEngine"
-         placeholder="Enter a city"
-         aria-label="Recipient's username with two button addons"
-          onChange={updateCity}
-        />
-        <input type="submit" value="Search"  className=" search"/>
-      </form>
-
-      {weatherData && (
-        <div>
-          <h1>Weather in {weatherData.city}</h1>
-          <ul>
-            {weatherItems.map((item, index) => (
-              <li key={index}>
-                <strong>{item.label}:</strong> {item.value}
-              </li>
-            ))}
-          </ul>
-          <img src={weatherData.icon} alt={weatherData.description} />
-        </div>
-      )}
-    </div>
-  );
+    } else {
+        const url = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=21b0o158462fba6c2a0f6eca1t37c8ec&units=metric`;
+        axios.get(url).then(handleResponse);
+    }
 }
+
+
+
+
+
