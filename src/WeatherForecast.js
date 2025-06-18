ff32470451ab466ab3112ac1fc4bc8c805062fab
MariyaSemiryazhko <semiryazhkomasha@gmail.com>
2025-06-18 15:45:34 +0200

    add a forecast api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 
 
-export default function WeatherForecast(props){
+export default function WeatherForecast(props) {
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+
+
+    let city = (props.data.city);
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=21b0o158462fba6c2a0f6eca1t37c8ec&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
     return (
         <div className="weather-forecast" id="forecast">
             <div className="row">
